bfe8948a5b1933b61b208597bd56bc2fafa3b8d6
Edson Martins <edsonmartins2005@gmail.com>
2026-07-24 18:07:31 -0300

    fix(db): persist runtime grants in schema

--- server/postgres/init.sql
+++ server/postgres/init.sql
@@ -250,6 +250,20 @@ EXCEPTION
 END
 $$;
 
+-- Runtime role grants when an administrative role applies this schema.
+DO $$
+BEGIN
+    IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'zaplivre') THEN
+        GRANT USAGE ON SCHEMA public TO zaplivre;
+        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO zaplivre;
+        GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO zaplivre;
+        GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO zaplivre;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO zaplivre;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO zaplivre;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO zaplivre;
+    END IF;
+END $$;
+
 -- Schedule cleanup job (if pg_cron available)
 DO $$
 BEGIN
